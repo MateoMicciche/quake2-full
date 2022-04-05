12f2b287ce3e4040e6d69fd482cc0ad7b0f26956
MateoMicciche <mateo.micciche@gmail.com>
2022-04-04 20:32:32 -0400

    Quake 2 Op weapon exercise. Made Railgun have inf ammo, shoot multiple shots, and faster fire rate

--- game/p_weapon.c
+++ game/p_weapon.c
@@ -829,7 +829,7 @@ void Blaster_Fire (edict_t *ent, vec3_t g_offset, int damage, qboolean hyper, in
 	VectorScale (forward, -2, ent->client->kick_origin);
 	ent->client->kick_angles[0] = -1;
 
-	fire_blaster (ent, start, forward, damage, 1000, effect, hyper);
+	fire_blaster (ent, start, forward, damage, 2, effect, hyper);
 
 	// send muzzle flash
 	gi.WriteByte (svc_muzzleflash);
@@ -1315,19 +1315,19 @@ void weapon_railgun_fire (edict_t *ent)
 	else
 	{
 		damage = 150;
-		kick = 250;
+		kick = 0;
 	}
 
 	if (is_quad)
 	{
 		damage *= 4;
-		kick *= 4;
+		kick *= 0;
 	}
 
 	AngleVectors (ent->client->v_angle, forward, right, NULL);
 
-	VectorScale (forward, -3, ent->client->kick_origin);
-	ent->client->kick_angles[0] = -3;
+	VectorScale (forward, 0, ent->client->kick_origin);
+	ent->client->kick_angles[0] = 0;
 
 	VectorSet(offset, 0, 7,  ent->viewheight-8);
 	P_ProjectSource (ent->client, ent->s.origin, offset, forward, right, start);
@@ -1342,17 +1342,17 @@ void weapon_railgun_fire (edict_t *ent)
 	ent->client->ps.gunframe++;
 	PlayerNoise(ent, start, PNOISE_WEAPON);
 
-	if (! ( (int)dmflags->value & DF_INFINITE_AMMO ) )
-		ent->client->pers.inventory[ent->client->ammo_index]--;
+	//if (! ( (int)dmflags->value & DF_INFINITE_AMMO ) )
+		//ent->client->pers.inventory[ent->client->ammo_index]--;
 }
 
 
 void Weapon_Railgun (edict_t *ent)
 {
 	static int	pause_frames[]	= {56, 0};
-	static int	fire_frames[]	= {4, 0};
+	static int	fire_frames[]	= {4,5,6,7, 0};
 
-	Weapon_Generic (ent, 3, 18, 56, 61, pause_frames, fire_frames, weapon_railgun_fire);
+	Weapon_Generic (ent, 3, 8, 56, 61, pause_frames, fire_frames, weapon_railgun_fire);
 }
 
 
